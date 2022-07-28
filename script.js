5fc0e61d8c37e94af6ff7c824bfa965d33e08ad4
SherryAngle <sherryangleWD@gmail.com>
2022-07-28 12:27:56 -0500

    add modified files

---
+++ script.js
@@ -0,0 +1,63 @@
+//exercise 1
+
+let a = 20;
+let b = 4;
+let sum = a + b;
+console.log(a + b)
+24
+let minus = a -b;
+console.log(a -b);
+16
+let multiply = a * b;
+console.log(a * b);
+80
+let divide = a / b;
+console.log(a / b);
+5
+
+//exercise 2
+
+//let num = 11;
+//let str = "11";
+//let str2 = "eleven";
+//let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log(11 + "11")
+//1111
+console.log(11 + "eleven")
+//'11eleven'
+console.log(11 + true) 
+//12
+console.log("Frodo" + 11)
+//Frodo11
+console.log(true + "11")
+//true11
+console.log("Frodo" + "Baggins")
+//FrodoBaggins
+
+//exercise 3
+
+let val = 5;
+let str = "5";
+let str2 = "five";
+let isPresent = false;
+
+console.log(5 == "5");
+//true
+console.log(5 === "5");
+//false
+console.log(!false);
+//true
+console.log("eleven" == "five" && 5>= "five");
+//false
+console.log(!false || false);
+//true
+console.log(0 == false);
+//true
+console.log(0 === false);
+//false
+console.log(0 != false);
+//false
+console.log(0 !== false)
